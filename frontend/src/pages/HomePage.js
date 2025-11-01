e9219df422e1cfd102d8d5a3136f4a2e3171472c
HawiGenene1 <hawigenene2u@gmail.com>
2025-11-01 08:31:13 +0300

    Initial MediLink project setup

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Welcome to MediLink</h2>
+      <p>Search and order medicines from trusted pharmacies.</p>
+    </div>
+  );
+}
+
+export default HomePage;
