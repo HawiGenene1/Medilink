da3307c47f97d5f8ea80d1ef9268e7c5a9b3b1de
HawiGenene1 <hawigenene2u@gmail.com>
2025-11-04 11:38:39 +0300

    Add complete frontend directory structure

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 import cors from "cors";
+import connectDB from "./src/config/db.js";
+
+console.log(" server.js file started running...");
 
 dotenv.config();
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB
+connectDB();
+
+// Default Route
 app.get("/", (req, res) => {
-  res.send("MediLink backend is running...");
+  res.send("✅ MediLink Backend is Running...");
 });
 
+// Server Listen
 const PORT = process.env.PORT || 5000;
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  
-  .catch((err) => console.error(err));
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
