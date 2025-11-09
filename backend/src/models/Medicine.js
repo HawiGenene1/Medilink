54130d576104f5ca00aa47d7dc249ca7d3210203
Bitanya Wondimagegn <bethanywondimagegn@gmail.com>
2025-11-09 23:24:28 +0300

    Create Medicine model with schema definition

---
+++ backend/src/models/Medicine.js
@@ -0,0 +1,30 @@
+// models/Medicine.js
+import mongoose from "mongoose";
+
+const medicineSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  expiryDate: {
+    type: Date,
+  },
+});
+
+const Medicine = mongoose.model("Medicine", medicineSchema);
+export default Medicine;
